6baeadc8f9ef6cf097507d498add7e99c6e91346
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-05-25 10:36:32 +0300

    add transparency base
    Есть какой-то баг с отображением прозрачности, несмотря на смену режима рендеринга, работает не очень корректно

--- SGS_WormHole/Assets/Character Movement Fundamentals/Source/Scripts/Skills/Platform.cs
+++ SGS_WormHole/Assets/Character Movement Fundamentals/Source/Scripts/Skills/Platform.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 namespace CMF
 {
+    [RequireComponent(typeof(Material))]
+
     public class Platform : MonoBehaviour
     {
         [SerializeField] private float _platformDistance;
         [SerializeField] private float _defaultHeight;
         [SerializeField] private float _timeBeforeHide;
+        [SerializeField] private Color _NormalColor;
+        [SerializeField] private Color _TransporentColor;
+        [SerializeField] private Material _material;
         
         public float LifeSeconds;
         
@@ -16,6 +21,9 @@ namespace CMF
         {
             transform.position = tr.position + tr.rotation * Vector3.forward * _platformDistance + Vector3.up * _defaultHeight;
             LifeSeconds = _timeBeforeHide;
+
+            _material.SetFloat("_Mode", 0f);
+            _material.SetColor("_Color", _NormalColor);
             gameObject.SetActive(true);
             
             StartCoroutine(HideAfterDelay());
@@ -24,6 +32,9 @@ namespace CMF
         public void ShowPlatformTarget(Transform tr)
         {
             transform.position = tr.position + tr.rotation * Vector3.forward * _platformDistance + Vector3.up * _defaultHeight;
+
+            _material.SetFloat("_Mode", 2f);
+            _material.SetColor("_Color",_TransporentColor);
             gameObject.SetActive(true);
         }
 
