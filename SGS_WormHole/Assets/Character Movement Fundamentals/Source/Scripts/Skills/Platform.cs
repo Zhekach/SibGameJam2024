81ea6e219d8f157a33db2f15203bd2f57c0ca691
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-05-28 14:21:02 +0300

    fix transparecy of platform
    Теперь полупрозрачность работает на двух материалах. Один нормальный, второй полупрозрачный. И меняются они, а не параметры одного материала

--- SGS_WormHole/Assets/Character Movement Fundamentals/Source/Scripts/Skills/Platform.cs
+++ SGS_WormHole/Assets/Character Movement Fundamentals/Source/Scripts/Skills/Platform.cs
@@ -11,20 +11,29 @@ namespace CMF
         [SerializeField] private float _platformDistance;
         [SerializeField] private float _defaultHeight;
         [SerializeField] private float _timeBeforeHide;
-        [SerializeField] private Color _NormalColor;
-        [SerializeField] private Color _TransporentColor;
+        //[SerializeField] private Color _NormalColor;
+        //[SerializeField] private Color _TransporentColor;
         [SerializeField] private Material _material;
-        
+        [SerializeField] private Material _materialTransparent;
+
         public float LifeSeconds;
         
+        private MeshRenderer _renderer;
+
+        private void Start()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+        }
+
         public void PlacePlatform(Transform tr)
         {
             transform.position = tr.position + tr.rotation * Vector3.forward * _platformDistance + Vector3.up * _defaultHeight;
             LifeSeconds = _timeBeforeHide;
 
-            _material.SetFloat("_Mode", 3f);
-            _material.SetColor("_Color", _NormalColor);
+            //_material.SetFloat("_Mode", 3f);
+            //_material.SetColor("_Color", _NormalColor);
             gameObject.SetActive(true);
+            _renderer.material = _material;
             
             StartCoroutine(HideAfterDelay());
         }
@@ -33,9 +42,10 @@ namespace CMF
         {
             transform.position = tr.position + tr.rotation * Vector3.forward * _platformDistance + Vector3.up * _defaultHeight;
 
-            _material.SetFloat("_Mode", 3f);
-            _material.SetColor("_Color",_TransporentColor);
+            //_material.SetFloat("_Mode", 3f);
+            //_material.SetColor("_Color",_TransporentColor);
             gameObject.SetActive(true);
+            _renderer.material = _materialTransparent;
         }
 
         private IEnumerator HideAfterDelay()
