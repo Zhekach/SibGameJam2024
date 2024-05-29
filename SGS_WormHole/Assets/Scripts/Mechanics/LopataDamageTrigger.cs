a2bbe5d707537fe76eedda684022a0967a39dcc5
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-05-29 14:33:38 +0300

    fix lopataDamage

--- SGS_WormHole/Assets/Scripts/Mechanics/LopataDamageTrigger.cs
+++ SGS_WormHole/Assets/Scripts/Mechanics/LopataDamageTrigger.cs
@@ -15,9 +15,10 @@ public class LopataDamageTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        isFighting = fightController.IsFighting;
+
         if(other.gameObject.TryGetComponent(out EnemyHPSystem playerHpSystem) && isFighting)
         {
-            isFighting = fightController.IsFighting;
 
             playerHpSystem.OnDemage?.Invoke(damageValue);
         }
@@ -25,9 +26,10 @@ public class LopataDamageTrigger : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        isFighting = fightController.IsFighting;
+
         if (collision.gameObject.TryGetComponent(out EnemyHPSystem playerHpSystem) && isFighting)
         {
-            isFighting = fightController.IsFighting;
 
             playerHpSystem.OnDemage?.Invoke(damageValue);
         }
